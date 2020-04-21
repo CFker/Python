d566411eed2ff30efb4efa392fb0cdc9e5af15ad
DevOp <137710367@qq.com>
2020-04-21 22:39:26 +0800

    test

---
+++ Date_visualization/sactter_squares.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+x_value = list(range(1, 5000))
+print(x_value)
+y_value = [x**3 for x in x_value]
+print(y_value)
+plt.scatter(x_value, y_value, c=y_value, edgecolors='none', s=20)
+
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+plt.tick_params(axis='both', which='major', labelsize=14)
+# plt.axis([0, 5000, 0, 11000000])
+
+plt.show()
